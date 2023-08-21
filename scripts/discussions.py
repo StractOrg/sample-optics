7321a87bbcda64544df8cb368d382b2299639e7a
Mikkel Denker <mikkel@trystract.com>
2023-08-21 09:16:55 +0200

    forgot to extract lemmy sites from urls

--- scripts/discussions.py
+++ scripts/discussions.py
@@ -13,16 +13,17 @@ instances = df["Instance"].tolist()
 
 # hacky but works
 urls = [instance.split("](")[1][:-1] for instance in instances]
+sites = [url.split("//")[1].split("/")[0] for url in urls]
 
 
-def rule(url):
+def rule(site):
     s = """Rule {{
     Matches {{
         Site("|{0}|"),
         Url("post"),
     }}
 }};""".format(
-        url
+        site
     )
 
     return s
@@ -54,6 +55,6 @@ Rule {
 optic += (
     "// source of instances: https://github.com/maltfield/awesome-lemmy-instances\n"
 )
-optic += "\n\n".join([rule(url) for url in urls])
+optic += "\n\n".join([rule(site) for site in sites])
 
 print(optic)
