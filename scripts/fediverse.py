e470feffd45c237f74953a1f58d7a2e101863272
Mikkel Denker <mikkel@trystract.com>
2023-12-08 16:08:12 +0100

    make fediverse optic faster

--- scripts/fediverse.py
+++ scripts/fediverse.py
@@ -5,28 +5,36 @@ import json
 file_path = Path(os.path.realpath(__file__))
 
 # source of nodes list: https://nodes.fediverse.party/nodes.json
-with open(file_path.parent.parent.joinpath("data").joinpath("fediverse_nodes.json")) as f:
+with open(
+    file_path.parent.parent.joinpath("data").joinpath("fediverse_nodes.json")
+) as f:
     sites = json.loads(f.read())
 
-def rule(site):
-    s = """Rule {{
-    Matches {{
-        Site("{0}")
-    }}
-}};""".format(
-        site
-    )
 
-    return s
+def rule(site):
+    if site.count(".") > 1:
+        return """Rule {{
+        Matches {{
+            Site("|{0}|")
+        }}
+    }};""".format(
+            site
+        )
+    else:
+        return """Rule {{
+        Matches {{
+            Domain("|{0}|")
+        }}
+    }};""".format(
+            site
+        )
 
 
 optic = """DiscardNonMatching;
 
 """
 
-optic += (
-        "// source of fediverse sites: https://nodes.fediverse.party/nodes.json\n"
-)
+optic += "// source of fediverse sites: https://nodes.fediverse.party/nodes.json\n"
 optic += "\n\n".join([rule(site) for site in sites])
 
 print(optic)
